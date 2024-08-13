1db4e7fae984a8be0dd6d335f7b6692bb5e9d5bb
nick <nick.m.dodson@gmail.com>
2024-08-13 19:01:42 -0400

    docs landing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,44 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        'fadeIn': 'fadeIn 150ms ease-in-out',
+        'bigPhoto': 'bigPhoto 2s ease-in-out',
+        'nogglePhoto': 'nogglePhoto 2.8s ease-in-out',
+      }
     },
+    keyframes: {
+      fadeIn: {
+        '0%': {
+          opacity: '0'
+        },
+        '100%': {
+          opacity: '1'
+        }
+      },
+      bigPhoto: {
+        '0%': {
+          opacity: '0'
+        },
+        '50%': {
+          opacity: '1'
+        },
+        '100%': {
+          opacity: '0'
+        }
+      },
+      nogglePhoto: {
+        '0%': {
+          opacity: '0'
+        },
+        '50%': {
+          opacity: '0'
+        },
+        '100%': {
+          opacity: '1'
+        }
+      },
+    }
   },
   plugins: [],
 };
